22ee7a44ae031505a0f309cf7e465eb969f13321
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-02 16:19:36 -0300

    Se agrega componente clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/components/home/home.component';
 import { AboutComponent } from '../app/components/about/about.component';
 import { ProveedoresComponent } from '../app/components/proveedores/proveedores.component';
+import { ClientesComponent } from '../app/components/clientes/clientes.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'representadas', component: ProveedoresComponent },
+  { path: 'clientes', component: ClientesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
