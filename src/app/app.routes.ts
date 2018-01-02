1de61971f1edd2dc8edc12048aa3080b1b2dc79f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-02 11:56:08 -0300

    Componente proveedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/components/home/home.component';
 import { AboutComponent } from '../app/components/about/about.component';
+import { ProveedoresComponent } from '../app/components/proveedores/proveedores.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'representadas', component: ProveedoresComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
