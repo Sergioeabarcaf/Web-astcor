2dc2adf274b0cad735f937cd64641a670a67491c
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-02 23:18:19 -0300

    se agrega clientes a rutas y navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from '../app/components/home/home.component';
 import { AboutComponent } from '../app/components/about/about.component';
 import { ProveedoresComponent } from '../app/components/proveedores/proveedores.component';
 import { ClientesComponent } from '../app/components/clientes/clientes.component';
+import { ProductosComponent } from '../app/components/productos/productos.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'representadas', component: ProveedoresComponent },
   { path: 'clientes', component: ClientesComponent },
+  { path: 'productos', component: ProductosComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
